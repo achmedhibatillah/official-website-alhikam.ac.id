7c9b92c824d39fb16c7c4b6e21a51ce5f68c6128
ach.hibatillah <ach.hibatillah@gmail.com>
2025-02-09 05:55:40 +0700

    Update

--- app/Views/user/pengumuman.php
+++ app/Views/user/pengumuman.php
@@ -6,22 +6,28 @@
 
     <div class="col-md-6 m-0 p-0 px-3 px-md-3">
     <div class="card bg-clr4 m-1 m-md-4 p-3">
-        <p class="fw-bold text-center mb-1 text-clr1">Surat kelulusan:</p>
-        <a href="<?= base_url('') ?>" class="btn btn-clr1 lh-1 px-3">Download surat kelulusan</a>
-        <div class="card m-0 p-0 border-clr1 mt-3 px-3 py-3 text-clr1">
-            <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
-            <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
-            <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
-            <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
-            <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
-            <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
-            <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
-            <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
-            <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
-            <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
-            <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
-            <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
+        <?php if ($pengumuman['pengumuman_saved'] == 1): ?>
+            <p class="fw-bold text-center mb-1 text-clr1">Pengumuman:</p>
+            <a href="<?= base_url('') ?>" class="btn btn-clr1 lh-1 px-3"><i class="fas fa-download me-2"></i> Download SK</a>
+            <div class="card m-0 p-0 border-clr1 mt-3 px-3 py-3 text-clr1">
+                <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
+                <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
+                <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
+                <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
+                <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
+                <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
+                <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
+                <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
+                <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
+                <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
+                <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
+                <p class="ls-1 m-0"><i class="fas fa-check-circle me-2"></i> Kasur</p>
         </div>
+        <?php else: ?>
+            <div class="d-flex justify-content-center align-items-center" style="min-height:200px;">
+                <p class="text-clr1 ls-1 text-center">Pengumuman belum keluar.</p>
+            </div>
+        <?php endif; ?>
     </div>
 
     </div>
